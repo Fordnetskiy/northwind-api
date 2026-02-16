3e7c7f1500f1b352a937ad17ebf6e93bd91ec9de
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 14:01:00 +0200

    feat: add logic for GET routes

--- src/service/prod.service.js
+++ src/service/prod.service.js
@@ -1,5 +1,24 @@
 const db = require("../config/database");
 
-class ProdService {}
+class ProdService {
+  getAll = async () => {
+    const result = db.query(`
+      SELECT * FROM products
+      LIMIT 20
+    `);
+    return result;
+  };
+
+  getOne = async (id) => {
+    const result = db.query(
+      `
+        SELECT * FROM products
+        WHERE product_id = $1
+        `,
+      [id],
+    );
+    return result;
+  };
+}
 
 module.exports = new ProdService();
