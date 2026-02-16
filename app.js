d5aefa39fe4ebdc9c143892a2b393fe7bdfdb388
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 13:18:30 +0200

    added api/v1 route, and error handler

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const routes = require("./src/router/index");
+const errorHandler = require("./src/middlewares/gErrHandler");
 
 const app = express();
 
 app.use(express.json());
+app.use("api/v1", routes);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
