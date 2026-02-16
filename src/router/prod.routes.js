0e2749523ff1b14243d243883c3a9d3c5edcb0f8
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 13:27:18 +0200

    connect controller into router

--- src/router/prod.routes.js
+++ src/router/prod.routes.js
@@ -1,13 +1,15 @@
 const { Router } = require("express");
 const router = Router();
 
+const ProdController = require("../controller/prod.controller");
+
 // C.R.U.D routes ===
 
 // Create
 router.post("/");
 
 // Read
-router.get("/");
+router.get("/", ProdController.getAll);
 router.get("/:id");
 
 // update
