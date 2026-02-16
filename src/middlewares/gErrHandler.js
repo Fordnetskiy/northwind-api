2a6f428c74716c7fc7ee1dfca3c2ee3c4e14b567
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 16:44:38 +0200

    fix: Fixed my stupid mistake with err

--- src/middlewares/gErrHandler.js
+++ src/middlewares/gErrHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-  const status = req.status || 500;
-  const message = req.message || "Internal Server Error";
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
   res.status(status).json({
     success: false,
     message: message,
