4faa024e8f6e42d92ea23833f3ebcfa65a657871
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 13:21:14 +0200

    Added CRUD routes for /products

--- src/router/prod.routes.js
+++ src/router/prod.routes.js
@@ -1,4 +1,21 @@
 const { Router } = require("express");
 const router = Router();
 
+// C.R.U.D routes ===
+
+// Create
+router.post("/");
+
+// Read
+router.get("/");
+router.get("/:id");
+
+// update
+router.put("/:id");
+
+// Delete
+router.delete("/:id");
+
+// ===
+
 module.exports = router;
