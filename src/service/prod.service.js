05ef36dff415112fba5c10094fdb12aea7c5ddee
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 14:05:58 +0200

    feat: added basic pagination

--- src/service/prod.service.js
+++ src/service/prod.service.js
@@ -1,11 +1,20 @@
 const db = require("../config/database");
 
 class ProdService {
-  getAll = async () => {
-    const result = db.query(`
-      SELECT * FROM products
-      LIMIT 20
-    `);
+  getAll = async (q) => {
+    // pagiantion
+    const { page = 1, limit = 10 } = q;
+    const offset = (page - 1) * limit;
+
+    const result = db.query(
+      `
+      SELECT product_id, product_name, unit_price 
+      FROM products
+      OFFSET $1
+      LIMIT $2
+    `,
+      [offset, limit],
+    );
     return result;
   };
 
