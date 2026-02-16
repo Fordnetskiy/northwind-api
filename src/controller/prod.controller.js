9f4caa7baa770953ef5759d6d64e9147aaf98ed1
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 13:52:44 +0200

    Add getOne ligic, and try/catch blocks

--- src/controller/prod.controller.js
+++ src/controller/prod.controller.js
@@ -3,15 +3,39 @@ const db = require("../config/database");
 class ProdController {
   // Read all
   getAll = async (req, res, next) => {
-    const result = await db.query(`
+    try {
+      const result = await db.query(`
       SELECT * FROM products
       LIMIT 20
     `);
 
-    res.json({
-      success: true,
-      data: result.rows,
-    });
+      res.json({
+        success: true,
+        data: result.rows,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  // Read one
+  getOne = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const result = await db.query(
+        `
+        SELECT * FROM products
+        WHERE product_id = $1
+        `,
+        [id],
+      );
+
+      res.json({
+        success: true,
+        data: result.rows[0],
+      });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
