9ed1c99e3083387e2620a038f171443ede1d6316
fordnetskiy <gtashnik22848@gmail.com>
2026-02-16 14:24:35 +0200

    feat: added page counter

--- src/service/prod.service.js
+++ src/service/prod.service.js
@@ -3,19 +3,38 @@ const db = require("../config/database");
 class ProdService {
   getAll = async (q) => {
     // pagiantion
-    const { page = 1, limit = 10 } = q;
+    const page = parseInt(q.page) || 1;
+    const limit = parseInt(q.limit) || 10;
     const offset = (page - 1) * limit;
 
-    const result = db.query(
-      `
-      SELECT product_id, product_name, unit_price 
-      FROM products
-      OFFSET $1
-      LIMIT $2
-    `,
-      [offset, limit],
-    );
-    return result;
+    const [prodRes, countRes] = await Promise.all([
+      db.query(
+        `
+        SELECT product_id, product_name, unit_price
+        FROM products
+        OFFSET $1
+        LIMIT $2
+      `,
+        [offset, limit],
+      ),
+      db.query(`
+        SELECT COUNT(*)
+        FROM products
+      `),
+    ]);
+
+    const totalItems = parseInt(countRes.rows[0].count);
+    const totalPages = Math.ceil(totalItems / limit);
+
+    return {
+      products: prodRes.rows,
+      pagination: {
+        totalItems,
+        totalPages,
+        currentPage: page,
+        itemsPerPage: limit,
+      },
+    };
   };
 
   getOne = async (id) => {
