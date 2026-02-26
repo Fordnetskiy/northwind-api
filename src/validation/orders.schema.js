5500a7041cf445683a6113ddce2063a46c01896e
fordnetskiy <linik.yaroslav@gmail.com>
2026-02-26 13:18:07 +0200

    Added validation schema for /orders

---
+++ src/validation/orders.schema.js
@@ -0,0 +1,26 @@
+const Joi = require("joi");
+
+const createOrderSchema = Joi.object({
+  customerId: Joi.string().required(),
+  employeeId: Joi.number().required(),
+  shipper: Joi.number().required(),
+  freight: Joi.number().required(),
+  address: Joi.string().min(10).max(60).required(),
+  city: Joi.string().min(2).max(40).required(),
+  country: Joi.string().min(3).max(30).required(),
+  postalCode: Joi.string().required().min(5).max(12),
+  productId: Joi.number().required(),
+  quantity: Joi.number().required(),
+});
+
+const updateOrderSchema = Joi.object({
+  requiredDate: Joi.date(),
+  shipper: Joi.number(),
+  address: Joi.string().min(10).max(60),
+  city: Joi.string().min(2).max(40),
+  region: Joi.string().min(2).max(40),
+  postalCode: Joi.number().min(5).max(12),
+  country: Joi.string().min(3).max(30),
+});
+
+module.exports = { createOrderSchema, updateOrderSchema };
