590a6928b5e6c9642389d97f11e962dc6a0f607f
fordnetskiy <linik.yaroslav@gmail.com>
2026-03-12 11:41:47 +0200

    feat: Swagger configuration was added

---
+++ src/config/swagger.js
@@ -0,0 +1,15 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Northwind API",
+      version: "1.1.0",
+    },
+  },
+  apis: [path.join(__dirname, "../docs/*.js")],
+};
+
+module.exports = swaggerJsdoc(options);
