5079add756e56b4048ddf745545e2b543b637b0a
fordnetskiy <linik.yaroslav@gmail.com>
2026-03-22 15:54:58 +0200

    changed experation time for token

--- src/utils/jwt.token.js
+++ src/utils/jwt.token.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "5m" });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 const verifyToken = (token) => {
